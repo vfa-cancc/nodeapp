aad379db40433ce3273fa9145ade933c23344ac2
Cu Cong Can <cancc@vitalify.asia>
2020-09-30 12:48:01 +0700

    change to port 3000

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ http.createServer(function (req, res) {
   console.log("incomming http request...");;
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Wellcome Boss v9');
-}).listen(3001);
+}).listen(3000);
 console.log("Server started!");;
